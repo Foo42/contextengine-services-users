c0c3766cce14af59e1efa7370fd344874c2e9dbc
Julian Haeger <julian.haeger@laterooms.com>
2014-02-05 13:07:26 +0000

    init

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-notify');
+
+  grunt.initConfig({
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['quickFeedback'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+  });
+
+  grunt.registerTask('quickFeedback', 'mochaTest');
+  grunt.registerTask('default', 'quickFeedback', 'watch');
+
+};
