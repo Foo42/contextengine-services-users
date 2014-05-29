fa42ba65cd19fada4120d588bf6220ce4f1363f8
Julian Haeger <julianhaeger@gmail.com>
2014-05-29 08:09:30 +0100

    state names can be edited

--- routes/config.js
+++ routes/config.js
@@ -9,23 +9,26 @@ configRoutes.getStateConfig = function(req,res){
 			console.log('could not retrieve config for user ' + req.user.id);
 			return res.end(500);
 		}
-		res.render('edit-state-config', {title:'Edit StateConfig', formattedConfig:JSON.stringify(stateConfig, null, 2)});
+		res.render('edit-state-config', {title:'Edit StateConfig', formattedConfig:JSON.stringify(stateConfig, null, 2), config:JSON.stringify(stateConfig)});
 	});
 }
 
 configRoutes.setStateConfig = function(req,res){
 	var newConfig;
 	try{
+		console.log('recieved config post=====================');
+		console.log('body: ' + JSON.stringify(req.body.data));
+		console.dir(req.body);
 		newConfig = JSON.parse(req.body.configJSON);
 	} catch (e){
-		return res.end(400);
+		return res.send(400);
 	}
 	
 	var configAccess = userConfigurationAccess.forUser(req.user);
 	console.log(JSON.stringify(newConfig));
 	configAccess.setStateConfig(newConfig, function(err){
 		if(err)	{
-			return res.end(400);
+			return res.send(400);
 		}
 		configRoutes.getStateConfig(req,res);
 	});
