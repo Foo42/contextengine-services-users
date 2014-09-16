41af29a0ad6aaedf0ef118485453bdb5f0f455be
Julian Haeger <julianhaeger@gmail.com>
2014-09-16 09:07:00 +0100

    began tidying queue reader code

--- core/contextEventBusReader.js
+++ core/contextEventBusReader.js
@@ -1,114 +1,73 @@
-	var amqp = require('amqp');
-	var Promise = require('promise');
+//////////////////////////////////////////////////////////////////////////////////////
 
-	var connectionSettings = {
-		host: process.env.RABBITMQ_HOST || '192.168.59.103',
-		login: 'admin',
-		password: 'aXo0o4BrUyUq'
-	};
+var EventEmitter = require('events').EventEmitter;
 
-	var exchangeSettings = {
-		type: 'fanout',
-		autoDelete: false
-	};
+var amqp = require('amqp');
+var Promise = require('promise');
 
-	var connection;
-	var recievingQueue;
+var connectionSettings = {
+	host: process.env.RABBITMQ_HOST || '192.168.59.103',
+	login: 'admin',
+	password: 'aXo0o4BrUyUq'
+};
 
-	getConnectionToExchange = function () {
-		return new Promise(function (resolve, reject) {
-			if (connection) {
-				return resolve(connection);
-			}
+var exchangeSettings = {
+	type: 'fanout',
+	autoDelete: false
+};
 
-			connection = amqp.createConnection(connectionSettings);
+var exchange;
 
-			connection.on('ready', connected);
-			connection.on('error', function (e) {
-				console.error('there was a problem with the rabbitmq connection for the context event bus reader: ' + JSON.stringify(e));
-				reject(new Error('problem with rabbit connection for reader'));
-			})
 
-			function connected() {
-				try {
-					console.log('reader connected to rabbitmq host');
-					connection.exchange('contextEvents', exchangeSettings, exchangeReady);
-				} catch (e) {
-					consle.error('problem with ready handler' + e);
-				}
-			}
-
-			function exchangeReady(contextEventsExchange) {
-				console.log('reader connected to contextEvents rabbitmq exchange');
-				resolve(connection);
-			};
-
-		});
-	}
-
-	module.exports = function (userId) {
-		//var amqp = require('amqp');
-
+function beginConnecting() {
+	return new Promise(function (resolve, reject) {
 		var connection = amqp.createConnection(connectionSettings);
-		connection.on('error', function (e) {
-			console.error('problem with rabbit connection ' + JSON.stringify(e))
-		});
-		connection.on('ready', function () {
-			console.log('connection ready');
-			connection.exchange('contextEvents', {
-				type: 'fanout',
-				autoDelete: false
-			}, function (exchange) {
-				console.log('exchange declared');
-				connection.queue('tmp-' + Math.random(), {
-						exclusive: true
-					},
-					function (queue) {
-						console.log('queue created');
-						queue.on('queueBindOk', function () {
-							console.log('queue bound');
+		connection.once('ready', resolve.bind(null, connection));
+	});
+}
 
+function declareExchange(connection) {
+	return new Promise(function (resolve, reject) {
+		connection.exchange('contextEvents', {
+			type: 'fanout',
+			autoDelete: false
+		}, resolve.bind(null))
+	});
+}
 
-							console.log('contextEventBusReader is listening')
-
-							queue.subscribe(function (msg) {
-								console.log(msg.data.toString('utf-8'));
-							});
-						});
-						queue.bind('contextEvents', '');
-
+var makingConnection = beginConnecting();
+var setupExchange = makingConnection.then(declareExchange);
+bindListenerQueue = setupExchange.then(function (exchange) {
+	console.log('listener exchange connect');
+	return new Promise(function (resolveListenerQueue) {
+		makingConnection.then(function (connection) {
+			connection.queue('tmp-' + Math.random(), {
+					exclusive: true
+				},
+				function queueCreated(queue) {
+					console.log('queue created');
+					queue.on('queueBindOk', function queueBound() {
+						resolveListenerQueue(queue);
 					});
-			});
+					queue.bind('contextEvents', '');
+				});
 		});
+	});
+});
 
-		// return getConnectionToExchange().then(function (connection) {
 
+///Need to store one per user n stuff
 
-
-		// 	function createABoundQueue(messageCallback) {
-		// 		console.log('about to create a boud queue ' + messageCallback);
-		// 		connection.queue('contextEventReaderQueue' + Math.random(), {
-		// 			exclusive: true
-		// 		}, function (queue, nextArg) {
-		// 			console.log('queue callback. queue = ' + JSON.stringify(queue) + ' next arg = ' + JSON.stringify(nextArg));
-		// 			console.log('contextEvent reader is binding to queue...');
-		// 			queue.bind('contextEvents', '', function (err) {
-		// 				if (err) {
-		// 					return console.error('error binding queue ' + JSON.stringify(err));
-		// 				}
-		// 				console.log('reader queue bound to contextEvents exchange');
-		// 				queue.subscribe(function (msg) {
-		// 					console.log('reader got context message ' + msg.data.toString('utf8'));
-		// 					if (messageCallback) {
-		// 						messageCallback(msg.data.toString('utf8'));
-		// 					}
-		// 				});
-		// 			});
-		// 		});
-		// 	};
-
-		// 	createABoundQueue();
-		// 	connection.on('ready', createABoundQueue);
-
-		// });
-	};
+module.exports = function (userId) {
+	console.log('in listener queue exports');
+	return bindListenerQueue.then(function (queue) {
+		console.log('listener queue bound');
+		var emitter = new EventEmitter();
+		queue.subscribe(function (msg) {
+			emitter.emit('dfsfsaf', msg.data.toString('utf-8'));
+			console.log(msg.data.toString('utf-8'));
+		});
+		console.log('subscribed to queue');
+		return emitter;
+	});
+};
