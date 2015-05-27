2737022ac27e0704f5321b9f272b680975ce4f10
Julian Haeger <julianhaeger@gmail.com>
2015-05-27 08:15:45 +0100

    Added colours to logging

--- core/logger.js
+++ core/logger.js
@@ -1,8 +1,19 @@
+var colors = require('colors/safe');
 var serviceName = process.env.SERVICE_NAME;
+
+function colourArguments(args, colour) {
+	return Array.prototype.slice.call(args).map(colors[colour]);
+}
 
 module.exports = {
 	log: console.log.bind(console, serviceName + ':'),
-	info: console.info.bind(console, serviceName + ':'),
-	error: console.error.bind(console, serviceName + ':'),
-	warn: console.warn.bind(console, serviceName + ':')
+	info: function () {
+		console.info.bind(console, serviceName + ':').apply(console, colourArguments(arguments, 'grey'));
+	},
+	error: function () {
+		console.error.bind(console, serviceName + ':').apply(console, colourArguments(arguments, 'red'));
+	},
+	warn: function () {
+		console.warn.bind(console, serviceName + ':').apply(console, colourArguments(arguments, 'orange'));
+	}
 }
